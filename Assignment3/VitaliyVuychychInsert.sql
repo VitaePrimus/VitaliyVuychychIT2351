a64e2ad6c770d7710e06364267081fabed2e7602
VitaePrimus <78100653+VitaePrimus@users.noreply.github.com>
2021-07-04 13:01:05 -0400

    Delete VitaliyVuychychInsert.sql

--- Assignment3/VitaliyVuychychInsert.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO customers
-VALUES (DEFAULT, 'myemail@gmail.com', 'RandomPassword!@123-pass', 'Vitaliy', 'Vuychych', DEFAULT, DEFAULT);
-SELECT * FROM customers;
-
-SELECT * INTO NewProductPrices
-FROM products;
